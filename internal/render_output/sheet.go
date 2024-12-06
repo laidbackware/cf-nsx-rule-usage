558f37458b97a7b5b540e08ae3e6544710d123e1
Matt Proud <laidbackware.git@gmail.com>
2024-12-06 13:30:42 +0000

    add formulas add summary of unused per asg

--- internal/render_output/sheet.go
+++ internal/render_output/sheet.go
@@ -1,8 +1,8 @@
 package render_output
 
 import (
+	"fmt"
 	"sort"
-	"strconv"
 
 	"github.com/laidbackware/cf-nsx-rule-usage/internal/collect_data"
 	"github.com/xuri/excelize/v2"
@@ -11,25 +11,24 @@ import (
 
 type sheetContent struct {
 	sheetName    string
-	sheetHeaders []string
+	sheetHeaders []excelize.Cell
 	columnWidths []float64
-	tableData    [][]string
+	tableData    [][]excelize.Cell
 }
 
 func WriteSheet(ruleUsage collect_data.RuleUsage, outputFile string) (err error) {
-	headers := []string{
-		"Foundation Name",
-		"ASG Name",
-		"Rule index",
-		"Target",
-		"Ports",
-		"Protocol",
-		"ASG Created",
-		"ASG Last Updated",
-		"Hit Count",
-		"Packet Count",
+	headers := []excelize.Cell{
+		{Value: "Foundation Name"},
+		{Value: "ASG Name"},
+		{Value: "Target"},
+		{Value: "Ports"},
+		{Value: "Protocol"},
+		{Value: "ASG Created"},
+		{Value: "ASG Last Updated"},
+		{Value: "Hit Count"},
+		{Value: "Packet Count"},
 	}
-	widths := []float64{25, 30, 10, 20, 20, 10, 20, 20, 15, 15}
+	widths := []float64{25, 30, 20, 20, 10, 20, 20, 15, 15}
 	allSheetsContents := []sheetContent{
 		{
 			sheetName:    "unused_rules",
@@ -42,6 +41,18 @@ func WriteSheet(ruleUsage collect_data.RuleUsage, outputFile string) (err error)
 			sheetHeaders: headers,
 			columnWidths: widths,
 			tableData:    buildTableArray(ruleUsage.AllRules),
+		},
+		{
+			sheetName: 		"ununsed_by_asg",
+			sheetHeaders: []excelize.Cell{
+				{Value: "Foundation Name"},
+				{Value: "ASG Name"},
+				{Value: "Used rules"},
+				{Value: "Total rules"},
+				{Value: "Used percentage"},
+			},
+			columnWidths: []float64{25, 30, 15, 15, 15},
+			tableData:    buildTableArrayUnusedCount(ruleUsage.AllRules),
 		},
 	}
 
@@ -78,32 +89,55 @@ func renderSheet(allSheetsContents []sheetContent, outputFile string) (error) {
 
 }
 
-func buildTableArray(sourceData map[string]map[string][]collect_data.Rule) (tableArray [][]string) {
+func buildTableArray(sourceData map[string]map[string][]collect_data.Rule) (tableArray [][]excelize.Cell) {
 	// Get keys to enable sorting so that each sheet has a predictable order
 	foundations := maps.Keys(sourceData)
 	sort.Strings(foundations)
-	idx := 0
 	for _, foundationName := range foundations {
 		asgs := maps.Keys(sourceData[foundationName])
 		sort.Strings(asgs)
 		for _, asgName := range asgs {
-			for ruleIdx, rule := range sourceData[foundationName][asgName] {
-				tableArray = append(tableArray, []string{
-					foundationName,
-					asgName,
-					strconv.Itoa(ruleIdx),
-					rule.Target,
-					rule.Ports,
-					rule.Protocol,
-					rule.Created,
-					rule.LastUpdated,
-					strconv.Itoa(rule.HitCount),
-					strconv.Itoa(rule.PacketCount),
+			for _, rule := range sourceData[foundationName][asgName] {
+				tableArray = append(tableArray, []excelize.Cell{
+					{Value: foundationName},
+					{Value: asgName},
+					{Value: rule.Target},
+					{Value: rule.Ports},
+					{Value: rule.Protocol},
+					{Value: rule.Created},
+					{Value: rule.LastUpdated},
+					{Value: rule.HitCount},
+					{Value: rule.PacketCount},
 				})
-				idx++
 			}
 		}
 	}
+	return
+}
+
+func buildTableArrayUnusedCount(sourceData map[string]map[string][]collect_data.Rule) (tableArray [][]excelize.Cell) {
+	// Get keys to enable sorting so that each sheet has a predictable order
+	foundations := maps.Keys(sourceData)
+	sort.Strings(foundations)
+	rowIdx := 1
+	for _, foundationName := range foundations {
+		asgs := maps.Keys(sourceData[foundationName])
+		sort.Strings(asgs)
+		for _, asgName := range asgs {
+			tableArray = append(tableArray, []excelize.Cell{
+				{Value: foundationName},
+				{Value: asgName},
+				{Formula: fmt.Sprintf("COUNTIFS(unused_rules!A:A,%s,unused_rules!B:B,%s)", getCellName(1, rowIdx+1), getCellName(2, rowIdx+1))},
+				{Formula: fmt.Sprintf("COUNTIFS(all_rules!A:A,%s,all_rules!B:B,%s)", getCellName(1, rowIdx+1), getCellName(2, rowIdx+1))},
+				{Formula: fmt.Sprintf("ROUND(%s/%s,2)*100", getCellName(3, rowIdx+1), getCellName(4, rowIdx+1))}})
+			rowIdx++
+		}
+	}
+	return
+}
+
+func getCellName(columnIdx, rolIdx int) (cellName string) {
+	cellName, _ = excelize.CoordinatesToCellName(columnIdx, rolIdx)
 	return
 }
 
@@ -116,9 +150,13 @@ func setColumnWidths(f *excelize.File, sheetName string, columnWidths []float64)
 }
 
 // Write line to a worksheet based on an array of strings
-func writeLine(f *excelize.File, sheetName string, content []string, rowIdx int) {
+func writeLine(f *excelize.File, sheetName string, content []excelize.Cell, rowIdx int) {
 	for columnIdx, cellContent := range content {
 		cellName, _ := excelize.CoordinatesToCellName(columnIdx+1, rowIdx+1)
-		_ = f.SetCellValue(sheetName, cellName, cellContent)
+		if cellContent.Value != nil {
+			_ = f.SetCellValue(sheetName, cellName, cellContent.Value)
+		} else {
+			_ = f.SetCellFormula(sheetName, cellName, cellContent.Formula)
+		}
 	}
 }
